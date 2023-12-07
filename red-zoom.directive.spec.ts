d420a1272f9c6bbc8b368fcbe9629d780413f540
Debanjan123 <debanjanmca123@gmail.com>
2023-12-07 10:41:36 +0530

    Add files via upload

---
+++ red-zoom.directive.spec.ts
@@ -0,0 +1,9 @@
+import { RedZoomDirective } from './red-zoom.directive';
+
+describe('RedZoomDirective', () => {
+    it('should create an instance', () => {
+        // @ts-ignore
+        const directive = new RedZoomDirective(null, null, null, null);
+        expect(directive).toBeTruthy();
+    });
+});
